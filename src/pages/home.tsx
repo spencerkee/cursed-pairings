bf0384f4b4688205b904596abcf1a0ee6f5c087f
spencerkee <spencerkee@users.noreply.github.com>
2025-09-03 22:52:32 +0000

    Add simple For

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,26 +1,46 @@
-import { Select, createOptions } from "@thisbeyond/solid-select";
+import { createOptions, Select } from "@thisbeyond/solid-select";
 import "@thisbeyond/solid-select/style.css";
+import { createEffect, createSignal, For } from "solid-js";
 
 export default function Home() {
-  const handleSelect = (obj) => {
-    console.log(obj);
-  }
+  const [selectedChar, setSelectedChar] = createSignal(null);
+  const [selectedPartners, setSelectedPartners] = createSignal([]);
 
-  const props = createOptions(
-    [
-      { name: "apple", pairs: [1, 2, 3] },
-      { name: "banana", pairs: [1, 2, 3] },
-      { name: "pear", pairs: [1, 2, 3] },
-      { name: "pineapple", pairs: [1, 2, 3] },
-      { name: "kiwi", pairs: [1, 2, 3] },
-    ],
-    { key: "name" }
-  );
+  createEffect(() => {
+    console.log('selectedChar', selectedChar());
+  });
+
+  // const handleSelect = (obj) => {
+  //   console.log(obj);
+  // }
+
+  let DATA_OBJ = new Map([
+    ["apple", { partners: ["banana", "pear"] }],
+    ["banana", { partners: ["apple", "kiwi"] }],
+    ["pear", { partners: ["apple", "pineapple"] }],
+    ["pineapple", { partners: ["pear"] }],
+    ["kiwi", { partners: ["banana"] }],
+  ]);
+
+  createEffect(() => {
+    if (selectedChar()) {
+      let partners = DATA_OBJ.get(selectedChar()).partners;
+      setSelectedPartners(partners);
+    } else {
+      setSelectedPartners([]);
+    }
+  });
+
+  // debugger;
+  const props = createOptions(Array.from(DATA_OBJ.keys()));
   return (
     <>
-      <Select {...props} onChange={handleSelect} />
+      <Select {...props} onChange={(obj) => setSelectedChar(obj)} />
+      <For each={selectedPartners()}>
+        {(item) => <div>{item}</div>}
+      </For>
       {/* <button class="btn">Button</button> */}
-      <div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+      {/* <div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
         <table class="table">
           <thead>
             <tr>
@@ -39,7 +59,7 @@ export default function Home() {
             </tr>
           </tbody>
         </table>
-      </div>
+      </div> */}
     </>
   )
   // onChange={(e) => console.log(e)}
