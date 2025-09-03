bd7722e3a267f245b265f47026963b56e2757698
spencerkee <spencerkee@users.noreply.github.com>
2025-09-03 21:41:55 +0000

    Add filtering and event handling

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,21 @@
 import { Select, createOptions } from "@thisbeyond/solid-select";
 import "@thisbeyond/solid-select/style.css";
-import { createSignal } from 'solid-js';
 
 export default function Home() {
-  const props = createOptions(["apple", "banana", "pear", "pineapple", "kiwi"]);
-    return <Select {...props} />;
+  const handleSelect = (e) => {
+    console.log(e);
+  }
+
+  const props = createOptions(
+    [
+      { name: "apple", pairs: [1, 2, 3] },
+      { name: "banana", pairs: [1, 2, 3] },
+      { name: "pear", pairs: [1, 2, 3] },
+      { name: "pineapple", pairs: [1, 2, 3] },
+      { name: "kiwi", pairs: [1, 2, 3] },
+    ],
+    { key: "name" }
+  );
+  return <Select {...props} onChange={handleSelect} />;
+  // onChange={(e) => console.log(e)}
 }
