a67257281c58bf12f4c777b3f4f3187012af0138
spencerkee <spencerkee@users.noreply.github.com>
2025-09-08 21:14:42 +0000

    Remove the rest of the router stuff now that the pages site works

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,3 @@
-// /* @refresh reload */
-// import './index.css';
-
-// import { render } from 'solid-js/web';
-
-// import { Router } from '@solidjs/router';
-// import App from './app';
-// import { routes } from './routes';
-
-// const root = document.getElementById('root');
-
-// if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
-//   throw new Error(
-//     'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
-//   );
-// }
-
-// render(
-//   () => <Router base={import.meta.env.SERVER_BASE_URL} root={(props) => <App>{props.children}</App>}>{routes}</Router>,
-//   root,
-// );
-
 /* @refresh reload */
 import { render } from 'solid-js/web';
 
